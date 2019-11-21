c77b7178b409470fd614e332161527f0fd7ac7fa
Timothy Claassens <tim.claassens@gmail.com>
2019-11-21 17:02:04 +1300

    Fix wrong path in import

--- src/domain/interface/program-request-body.interface.ts
+++ src/domain/interface/program-request-body.interface.ts
@@ -1,8 +1,8 @@
 import IProgram from './program';
-import { ProgramAction } from '../../../dist/domain/web-socket-message/body/program-request';
+import { PROGRAM_REQUEST_ACTION } from '../enum/program-request-action.enum';
 
 export default interface IProgramRequestBody {
-    action: ProgramAction;
+    action: PROGRAM_REQUEST_ACTION;
     program?: IProgram;
     programId?: string;
     boardId?: string;
